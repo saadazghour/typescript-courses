006c768257782d9551c9a60522d05948c5b89193
saadazghour <saad.azghour@gmail.com>
2024-05-04 18:40:35 +0100

    feat: ✔️ cast types to other types

--- packages/notes-ts-fundamentals-v4/src/03-variables-and-values.ts
+++ packages/notes-ts-fundamentals-v4/src/03-variables-and-values.ts
@@ -13,14 +13,13 @@ humidity = temperature //! (3) - number is not of type `79`
 humidity = 79 //✔️ (4) - 79 is of type `79`
 humidity = 78 //! (5) - 78 is not of type `79`
 
-/*
-// let temp2 = 19; //! temp2's type is { all numbers }
-// let humid2 = 79 as const; //! humidity's type is { 79 }
-// temp2 = 23; //! Is each member in { 23 } also in { all numbers }?
-// temp2 = humid2; //! Is each member in { 79 } also in { all numbers }?
-// humid2 = temp2; //! Is each member in { all numbers } also in { 79 }?
-// humid2 = 79; //! Is each member in { 79 } also in { 79 }
-// humid2 = 78; //! Is each member in { 78 } also in { 79 }
+let temp2 = 19 //! temp2's type is { all numbers }
+let humid2 = 79 as const //! humidity's type is { 79 }
+temp2 = 23 //! Is each member in { 23 } also in { all numbers }?
+temp2 = humid2 //! Is each member in { 79 } also in { all numbers }?
+humid2 = temp2 //! Is each member in { all numbers } also in { 79 }?
+humid2 = 79 //! Is each member in { 79 } also in { 79 }
+humid2 = 78 //! Is each member in { 78 } also in { 79 }
 
 //* Implicit `any` and type annotations
 /*
